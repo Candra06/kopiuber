882ec22d28d0321799bed7c02dff73c08c02e6a8
Candra06 <candrabiyu4@gmail.com>
2019-04-08 12:08:40 +0700

    logo

--- application/views/frontend/layout/header.php
+++ application/views/frontend/layout/header.php
@@ -4,7 +4,7 @@
       <div class="logo float-left">
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <h1 class="text-light"><a href="#header"><span>NewBiz</span></a></h1> -->
-        <a href="#intro" class="scrollto"><img src="<?= base_url() ?>asset/frontend/img/logo.png" alt="" style="width:180%;height:50%;"  class="img-fluid"></a>
+        <a href="#intro" class="scrollto"><img src="<?= base_url() ?>asset/frontend/img/logo.png" alt="" style="width:100px;height:50px;"  class="img-fluid"></a>
       </div>
 
       <nav class="main-nav float-right d-none d-lg-block">
